23238b3d138c5ea3926d8268e5eed150ba84f6ec
jesussosamartinez <ruizadrian455@gmail.com>
2022-12-05 20:06:42 -0600

    first commit

---
+++ assets/js/nav.js
@@ -0,0 +1,6 @@
+document.querySelector(".dropdown").addEventListener("click", function () {
+  const ico = document.querySelector("i");
+  console.log(ico);
+
+ 
+});
---
+++ sistema/departamentos.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link rel="stylesheet" href="../css/nav.css">
+</head>
+<body>
+<header>
+        <?php 
+    require 'header.html'
+    ?>
+    </header>
+
+    <div class="navegacion">
+        <a><i class="fa-solid fa-house"></i> <i class="fa-solid fa-chevron-right"></i>  Inicio <i class="fa-solid fa-chevron-right"></i> Departamentos</a>
+    </div>
+    <hr>
+</body>
+</html>
