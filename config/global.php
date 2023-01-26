8170eb19ac33fab01d2216585e770d12096f7cf9
irisZam1 <119894274+irisZam1@users.noreply.github.com>
2023-01-25 19:54:40 -0600

    Update global.php

--- config/global.php
+++ config/global.php
@@ -3,7 +3,7 @@
 define("DB_HOST","localhost");
 
 //Nombre de la base de datos 
-define("DB_NAME","sistema");
+define("DB_NAME","sistemaic");
 
 //Usuario de la Base de Datos
 define("DB_USERNAME", "root");
