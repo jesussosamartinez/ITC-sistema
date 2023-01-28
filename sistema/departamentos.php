80811edbd007129285f0bb3fa710ce512b0e6df6
irisZam1 <119894274+irisZam1@users.noreply.github.com>
2023-01-27 23:48:49 -0600

    Update departamentos.php

--- sistema/departamentos.php
+++ sistema/departamentos.php
@@ -23,17 +23,17 @@
 
 <!-- Departamentos menu -->
 
-    <section class="departamentos" style="top:45%; position:absolute; left:10%; z-index: -5; width: 70%">
+    <section class="departamentos" style="top:45%; position:absolut; left:10%; z-index:-5; width: 70%">
   
     <div class="accordion accordion-flush" id="accordionFlushExample">
   <div class="accordion-item">
     <h2 class="accordion-header" id="flush-headingOne">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-        Accordion Item #1
+        Dirección
       </button>
     </h2>
     <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+      <div class="accordion-body"> Jefe del Departamento -- M.C. Porfirio Roberto Nájera Medina <br>holaa</div>
     </div>
   </div>
   <!------------------------------------------------------------------------------------------------------------------->
