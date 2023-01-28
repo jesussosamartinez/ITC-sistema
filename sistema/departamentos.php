fcf83844f808bccdd2c81af5d690f2406f78e62f
irisZam1 <119894274+irisZam1@users.noreply.github.com>
2023-01-27 21:49:13 -0600

    Update departamentos.php

--- sistema/departamentos.php
+++ sistema/departamentos.php
@@ -24,18 +24,18 @@
 <!-- Departamentos menu -->
 
     <section class="departamentos" style="top:45%; position:absolute; left:10%; z-index: -5; width: 70%">
+  
     <div class="accordion accordion-flush" id="accordionFlushExample">
-
-    <div class="accordion-item">
+  <div class="accordion-item">
     <h2 class="accordion-header" id="flush-headingOne">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-      Departamento de Ciencias Basicas1
+        Accordion Item #1
       </button>
-      </h2>
+    </h2>
     <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
       <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
     </div>
-  </div> 
+  </div>
   <!------------------------------------------------------------------------------------------------------------------->
   <div class="accordion-item">
     <h2 class="accordion-header" id="flush-headingTwo">
