29b2a37034fd95774c906dbf3695e7a37f05157b
irisZam1 <119894274+irisZam1@users.noreply.github.com>
2023-02-19 21:31:49 -0600

    irs

---
+++ sistema/SelecTalleres.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Talleres Culturales- SIC</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+    <link rel="stylesheet" href="../dist/style.css">
+</head>
+<body>
+    <header>
+        <?php 
+    require 'header.php'
+    ?>
+    </header>
+    
+    <div class="navegacion">
+        <a><i class="fa-solid fa-house"></i><i class="fa-solid fa-chevron-right"></i> Seleccion de Talleres</a>
+    </div>
+    <hr>
+
+    <!-- Table Seleccion de actividades -->
+
+    <section class="departamentos" style="top:45%; position:absolute; left:10%; z-index:-5; width: 70%;">
+  
+  <div class="accordion accordion-flush" id="accordionFlushExample">
+<div class="accordion-item">
+<br><h5> DEPARTAMENTO DE ACTIVIDADES EXTRAESCOLARES </h5>
+  <h2 class="accordion-header" id="flush-headingOne">
+    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+      TALLERES DEPORTIVOS
+    </button>
+  </h2>
+  <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+  <div class="card-group">
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <button type="button" class="btn btn-primary">Primary</button>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <button type="button" class="btn btn-primary">Primary</button>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <button type="button" class="btn btn-primary">Primary</button>
+    </div>
+  </div>
+</div>
+  </div>
+</div>
+<!------------------TC--------------------->
+<br>
+<h2 class="accordion-header" id="flush-heading2">
+    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse2" aria-expanded="false" aria-controls="flush-collapse2">
+      TALLERES CULTURALES
+    </button>
+  </h2>
+  <div id="flush-collapse2" class="accordion-collapse collapse" aria-labelledby="flush-heading2" data-bs-parent="#accordionFlushExample">
+  <div class="card-group">
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+
+  </div>
+
+
+
+
+
+
+    </section>
+ 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>    
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+    <script src="../dist/script.js"></script>
+</body>
+</html>
