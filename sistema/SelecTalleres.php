aae5c0adde567113c5b9b4958ac28f2e8ff34afa
irisZam1 <119894274+irisZam1@users.noreply.github.com>
2023-03-13 20:47:17 -0600

    iris.3.0

--- sistema/SelecTalleres.php
+++ sistema/SelecTalleres.php
@@ -89,7 +89,7 @@ while($ver=mysqli_fetch_row($result)){
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title" id="exampleModalLabel"></h1>
+        <h5 class="modal-title" id="exampleModalLabel">Selección de Taller</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
