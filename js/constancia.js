d0654baaafc847d8d5f00c2f4c08645f628a4fc8
jesussosamartinez <80945919+jesussosamartinez@users.noreply.github.com>
2023-05-04 00:17:23 -0600

    sosa

--- js/constancia.js
+++ js/constancia.js
@@ -1,9 +1,26 @@
 
+
 function fnPDF(){
 
-var doc = new jsPDF();
+var doc = new                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        jsPDF();
 
-doc.text(20, 20, 'CONSTANCIA DE CUMPLIMIENTO');
+
+
+doc.setFontSize(14);
+doc.setTextColor(100);
+doc.setFontType("bold");
+doc.setFont("montserrat");
+doc.text(35, 30, 'LISTA DE ASISTENCIA DE ACTIVIDAD COMPLEMENTARIA');
+
+doc.html(document.querySelector('#tbllist'));
+
+
 
 doc.save('constancia.pdf');
+}
+
+function fnEvaluacionDesempeño(){
+    var formato = new jsPDF();
+
+    formato.text(20, 20, 'FORMATO DE EVALUACIÓN AL DESEMPEÑO DE LA ACTIVIDAD COMPLEMENTARIA')
 }
